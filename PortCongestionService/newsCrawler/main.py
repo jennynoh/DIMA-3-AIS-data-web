9de30f1deed15549f8be62e8bcab01f7571e913f
Harin <liv951230@gmail.com>
2024-04-17 14:56:27 +0900

    vesselSchedule.html에 데이터 크롤링해옴. 로딩창 추후 수정 필요(하린)

--- PortCongestionService/newsCrawler/main.py
+++ PortCongestionService/newsCrawler/main.py
@@ -9,8 +9,15 @@ from starlette.responses import JSONResponse
 import pickle
 import numpy as np
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-
+# ========================================== 뉴스 크롤러 ==========================================
 
 # 원하는 데이터를 추출하는 함수
 def extract_data(article):
@@ -67,47 +74,150 @@ def my_crawling() :
     return article_list
 
 
+# ========================================== 해상 스케줄 크롤러 ==========================================
+
+# 원하는 데이터를 추출하는 함수
+def extract_schdeule(schedule):
+
+    # 선사명 추출
+    company_name = schedule.find_element(By.CSS_SELECTOR, 'div.col-vessel p.port-name').text.strip()
+
+    # 선박명 추출
+    vessel_name = schedule.find_element(By.CSS_SELECTOR, 'div.col-vessel div.vessel-name-overcheck').text.strip()
+
+    # 출발항 추출
+    origin = schedule.find_element(By.CSS_SELECTOR, 'div.col2 p.port-name').text.strip()
+
+    # 도착항 추출
+    destination = schedule.find_element(By.CSS_SELECTOR, 'div.col3 p.port-name').text.strip()
+
+    # 출발일 추출
+    dep_time = schedule.find_element(By.CSS_SELECTOR, 'div.col2 p.date').text.strip()
+
+    # 도착일 추출
+    arr_time = schedule.find_element(By.CSS_SELECTOR, 'div.col3 p.date').text.strip()
+
+    # 소요시간 추출
+    duration = schedule.find_element(By.CSS_SELECTOR, 'div.col5 div.col-box').text.strip()
+
+    # 환적여부 추출
+    trans = schedule.find_element(By.CSS_SELECTOR, 'div.col6 span.trans-str').text.strip()
+
+    # 해상 운임비(O/F) 추출
+    price = schedule.find_elements(By.CSS_SELECTOR, 'div.currency-box span.price')
+    if len(price) > 1:
+        of20 = price[0].text.strip()
+        of40 = price[1].text.strip()
+    else:
+        of20, of40 = "", ""
+
+
+    return {
+        'company_name': company_name,
+        'vessel_name': vessel_name,
+        'origin' : origin,
+        'destination' : destination,
+        'dep_time' : dep_time,
+        'arr_time': arr_time,
+        'duration' : duration,
+        'trans': trans,
+        'of20' : of20,
+        'of40' : of40
+    }
+
+
+# 출발항, 도착항 정보 받아오기
+# depPort = '부산'
+# arrPort = '싱가포르'
+
+# 크롤링하는 함수
+def schedule_crawling(depPort,arrPort) :
+
+    # 크롤링할 페이지 url
+    url = 'https://www.tradlinx.com/schedule'
+
+    # 크롬 옵션 설정
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless")
+
+    # 속도 향상을 위해 이미지 로딩 차단 (1:이미지 로딩 허용/2:차단)
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    # 크롬 브라우저를 이용해 웹 브라우저 실행
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get(url)
+
+    # 웹 페이지에서 검색 입력 상자 찾기
+    search_boxes = driver.find_elements(By.CLASS_NAME, 'sm-input')
+    # search_box2 = driver.find_elements(By.CSS_SELECTOR, 'body > application > div > schedule > div > div.ocean-schedule-layer > div > div > div > div > div:nth-child(2) > port-selector > div > input')
+
+    # 첫 번째 검색어(출발항) 입력
+    search_boxes[0].send_keys(depPort)
+    search_boxes[0].send_keys(Keys.ENTER)
+
+    # 두 번째 검색어(도착항) 입력
+    search_boxes[1].send_keys(arrPort)
+    search_boxes[1].send_keys(Keys.ENTER)
+
+    # 검색 버튼을 클릭
+    # css 셀렉터로 찾음(f12에서 우클 - copy - copy selector)
+    search_button = driver.find_element(By.CSS_SELECTOR, 'body > application > div > schedule > div > div.ocean-schedule-layer > div > div > div > div > div.search-button-box > button')
+
+    search_button.click()
+    time.sleep(1)
+
+    # class가 'one-schedule fcl'인 li들 가져오기
+    schedules = driver.find_elements(By.CLASS_NAME, 'one-schedule')
+    print("크롤링 완료")
+
+    # 추출한 데이터를 담을 리스트
+    schedule_list = []
+
+    # 각 li에서 원하는 데이터 뽑기
+    for schedule in schedules:
+        result = extract_schdeule(schedule)
+        schedule_list.append(result)
+
+    print("데이터 추출 완료")
+
+    # 드라이버 종료
+    driver.quit()
+
+    return schedule_list
+
+
 # ========================================== fast api로 데이터 전송 ==========================================
 
-# Model 생성
-class Item(BaseModel) :
-    petalLength: float
-    petalWidth : float
-    sepalLength: float
-    sepalWidth: float
+# 스케줄 검색할 항구명을 입력받기 위한 Model 생성
+class Port(BaseModel) :
+    depPort: str
+    arrPort : str
 
-app = FastAPI()
+# 무조건 app이 맨 밑에 가게 해야함
+app = FastAPI() 
 
-@app.get(path="/", status_code=201)
-def myiris() :
+# 뉴스 전송하는 fast api
+@app.get(path="/news", status_code=201)
+async def myNews() :
                                  
     result = my_crawling()
     print("=========== 크롤링 결과 ===========", result)
 
     return JSONResponse(result)
 
+
+# 스케줄 전송하는 fast api
+
+@app.post(path="/schedule", status_code=201)
+async def mySchedule(port:Port) :
+    print("스케줄" , port.depPort , port.arrPort)
+    result = schedule_crawling(port.depPort , port.arrPort)
+    print("=========== 크롤링 결과 ===========", result)
+
+    return JSONResponse(result)
+
+
+
 if __name__ == '__main__' :
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
-
-
-
-
-
-
-# ================================================================================================================
-# 서버 URL
-#server_url = 'http://marineNews.com/send_data'
-
-# 보낼 데이터 JSON 형식으로 변환
-#data_json = json.dumps(article_list)
-
-# POST 요청 보내기
-#response = requests.post(url, data=data_json, headers={'Content-Type': 'application/json'})
-
-# 응답 확인
-#if response.status_code == 200:
-#    print('Data sent successfully')
-#else:
-#    print('Failed to send data. Status code:', response.status_code)
--- PortCongestionService/src/main/java/net/kdigital/portservice/controller/CrawlingController.java
+++ PortCongestionService/src/main/java/net/kdigital/portservice/controller/CrawlingController.java
@@ -6,16 +6,22 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.kdigital.portservice.dto.PortinfoDTO;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,7 +43,7 @@ public class CrawlingController {
 	@ResponseBody
 	public List<Map<String, String>> receive_data(@RequestParam(value="curr", defaultValue="1") int curr ) { //전체를 크롤링
 		log.info("crawling server: {}, {}", url, curr);
-		
+
 		List<Map<String, String>> list = new ArrayList<>();
 		if(curr == 1) {
 			crawlingAll();
@@ -48,7 +54,7 @@ public class CrawlingController {
 		} else if(curr == 2) {
 			list = new ArrayList<>();
 			for(int i=10; i<20; ++i) {
-				
+
 				list.add(result.get(i));
 			}
 		}
@@ -56,21 +62,75 @@ public class CrawlingController {
 		return list; // 정상반환
 	}
 
-	private void crawlingAll() { //전체를 크롤링
-		log.info("crawling server: {}", url);
 
+	@GetMapping("/port")
+	public String portInfo() {
+
+		return "vesselSchedule";
+	}
+	
+	private void crawlingAll() { //뉴스기사 전체 크롤링
+		log.info("crawling server: {}", url);
+		
 		//Map<String, Object> error = new HashMap<>();	// 에러 담을 맵
 		//Map<String, Object> result = new HashMap<>();	// 정상 데이터를 담을 맵
+		
+		
+		try {
+			result = restTemplate.getForObject(url+"/news", List.class);
+			
+			
+		} catch (HttpClientErrorException | HttpServerErrorException e) {
+		} 
+		
+		// result가 멤버변수로 선언되어서 반환할 필요x
+	}
 
+	// ============================================== 스케줄 크롤러 ==============================================
+	//항구 정보
+	@GetMapping("/port_info")
+	@ResponseBody
+	public List<Map<String, String>> port_info( // 출발항, 도착항 정보 받아오기
+			@ModelAttribute PortinfoDTO portInfo
+			) {
+		
+		log.info("항구 정보: {}", portInfo.toString());
+
+		List<Map<String, String>> res= scheduleCrawler(portInfo); // 스케줄 크롤링 함수 호출
+		
+
+		log.info("===========결과 : {}", res);
+		return res; // 정상반환
+	}
+
+
+	private List<Map<String,String>> scheduleCrawler(PortinfoDTO portInfo) { // 해상 스케줄 크롤링
+		log.info("crawling server: {}", url);
+		ResponseEntity<String> res = null;
+		List<Map<String, String>> dataList = null;
 
 		try {
-			result = restTemplate.getForObject(url, List.class);
+			// 정보를 보낼 때는 postForEntity를 써야 함
+			res = restTemplate.postForEntity(url+"/schedule", portInfo, String.class);
+			
+			log.info("res : {}",res);
 
+			// res는 json문자열이라 jackson을 이용해서 파싱해줘야 함
+			// ObjectMapper를 사용하여 JSON을 List<Map<String, String>>으로 변환
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        try {
+	            dataList = objectMapper.readValue(res.getBody(), List.class);
+	            
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            dataList = null;
+	            return dataList;
+	        }
 
 		} catch (HttpClientErrorException | HttpServerErrorException e) {
 		} 
 
-		// result가 멤버변수로 선언되어서 반환할 필요x
+		return dataList;
 	}
 
 }
